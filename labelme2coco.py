2c9a19a5bb27b75d45c5e1bd2a3c3f9e1c59ab16
Yang Wei <cablecomm@qq.com>
2020-07-30 11:40:23 +0800

    Update labelme2coco.py

--- labelme2coco.py
+++ labelme2coco.py
@@ -3,7 +3,7 @@
 
 import argparse
 import json
-
+import random
 import matplotlib.pyplot as plt
 import skimage.io as io
 import cv2
@@ -11,35 +11,60 @@ from labelme import utils
 import numpy as np
 import glob
 import PIL.Image
+import shutil
 from shapely.geometry import Polygon
+import os
 
 class labelme2coco(object):
-	def __init__(self,labelme_json=[],save_json_path='./new.json'):
+	def __init__(self,labelme_json=[],train_json_path='./tain2017.json',val_json_path='./val2017.json',test_json_path='./test2017.json'):
 		'''
 		:param labelme_json: all the labelme json files
-		:param save_json_path: the dir of saved json files
+		:param train_json_path: the path of saved train json file
+		:param val_json_path: the path of saved val json file 
+		:param test_json_path: the path of saved test json file
 		'''
 		self.labelme_json=labelme_json
-		self.save_json_path=save_json_path
-		self.images=[]
+		self.train_json_path=train_json_path
+		self.val_json_path=val_json_path
+		self.test_json_path=test_json_path
+		
+		self.train_images=[]
+		self.val_images=[]
+		self.test_images=[]
+		
 		self.categories=[]
-		self.annotations=[]
+		
+		self.train_annotations=[]
+		self.val_annotations=[]
+		self.test_annotations=[]
 		# self.data_coco = {}
 		self.label=[]
 		self.annID=1
 		self.height=0
 		self.width=0
-		
+
 		self.save_json()
-		
-		
+
+	
 	def data_transfer(self):
 		for num,json_file in enumerate(self.labelme_json):
 			print(json_file,num)
 			with open(json_file,'r') as fp:
 				data = json.load(fp)  # load the json files
 				#print(data)
-				self.images.append(self.image(data,num))
+				oldfile = "G:\\squidtap\\json\\"+data['imagePath'].split('/')[-1]
+				rand = random.randint(0, 9) #train [0,1,2,3,4,5],valid [6,7],test [8,9]
+				newfile = ''
+				if(rand>=0 and rand<=5):
+					self.train_images.append(self.image(data,num))
+					newfile = "G:\\squidtap\\images\\train2017\\"+data['imagePath'].split('/')[-1]
+				elif(rand>=6 and rand<=7):
+					self.val_images.append(self.image(data,num))
+					newfile = "G:\\squidtap\\images\\val2017\\"+data['imagePath'].split('/')[-1]
+				else:
+					self.test_images.append(self.image(data,num))
+					newfile = "G:\\squidtap\\images\\test2017\\"+data['imagePath'].split('/')[-1]
+				shutil.move(oldfile, newfile)
 				keypoints = []
 				val = 0
 				full_label = ''
@@ -65,8 +90,8 @@ class labelme2coco(object):
 						val = 8
 					#print("values ", val)
 					
-					if full_label not in self.label:
-						self.categories.append(self.categorie(full_label))
+					if ((full_label not in self.label) and full_label != "bottle"):
+						self.categories.append(full_label)
 						self.label.append(full_label)
 					
 					shapetype = shapes['shape_type']
@@ -81,11 +106,18 @@ class labelme2coco(object):
 					if shapetype == "rectangle":
 						points=shapes['points']
 						#print(points)
-					
-				self.annotations.append(self.annotation(points,full_label,num,keypoints))
+				
+				if(rand>=0 and rand<=5):
+					self.train_annotations.append(self.annotation(points,full_label,num,keypoints))
+				elif(rand>=6 and rand<=7):
+					self.val_annotations.append(self.annotation(points,full_label,num,keypoints))
+				else:
+					self.test_annotations.append(self.annotation(points,full_label,num,keypoints))
+				
 				self.annID+=1
         
 		print (num, json_file)
+
 
 	def image(self,data,num):
 		image={}
@@ -99,19 +131,10 @@ class labelme2coco(object):
 		image['width'] = width
 		image['id']=num+1
 		image['file_name'] = data['imagePath'].split('/')[-1]
-
 		self.height=height
 		self.width=width
 
 		return image
-
-
-	def categorie(self,label):
-		categorie={}
-		categorie['supercategory'] = "bottle"
-		categorie['id']=len(self.label)+1 # 0 is default as background
-		categorie['name'] = label
-		return categorie
 
 
 	def annotation(self,points,label,num,keypoints):
@@ -122,9 +145,9 @@ class labelme2coco(object):
 		annotation['iscrowd'] = 0
 		annotation['image_id'] = num+1
 
-		annotation['bbox'] = list(map(float,self.getbbox(points)))
+		annotation['bbox'] = list(map(int,self.getbbox(points)))
 
-		annotation['category_id'] = self.getcatid(label)
+		annotation['category_id'] = 1
 		annotation['id'] = self.annID
 
 		# Get the area value
@@ -145,6 +168,7 @@ class labelme2coco(object):
 		polygons = points
 		mask = self.polygons_to_mask([self.height,self.width], polygons)
 		return self.mask2box(mask)
+
 
 	def mask2box(self, mask):
 		'''从mask反算出其边框
@@ -176,25 +200,84 @@ class labelme2coco(object):
 		PIL.ImageDraw.Draw(mask).polygon(xy=xy, outline=1, fill=1)
 		mask = np.array(mask, dtype=bool)
 		return mask
-
-
-	def data2coco(self):
+	
+	
+	def train_data2coco(self):
 		data_coco={}
-		data_coco['images']=self.images
-		data_coco['categories']=self.categories
-		data_coco['annotations']=self.annotations
+		data_coco['images']=self.train_images
+		
+		categorie={}
+		categorie['supercategory'] = "bottle"
+		categorie['id']=1 # 0 is default as background
+		categorie['name'] = "bottle"
+		categorie['keypoints'] = self.categories
+		
+		data_coco['categories']=[categorie]
+		data_coco['annotations']=self.train_annotations
 		return data_coco
+		
+	
+	def val_data2coco(self):
+		data_coco={}
+		data_coco['images']=self.val_images
+		
+		categorie={}
+		categorie['supercategory'] = "bottle"
+		categorie['id']=1 # 0 is default as background
+		categorie['name'] = "bottle"
+		categorie['keypoints'] = self.categories
+		
+		data_coco['categories']=[categorie]
+		data_coco['annotations']=self.val_annotations
+		return data_coco
+
+
+	def test_data2coco(self):
+		data_coco={}
+		data_coco['images']=self.test_images
+		
+		categorie={}
+		categorie['supercategory'] = "bottle"
+		categorie['id']=1 # 0 is default as background
+		categorie['name'] = "bottle"
+		categorie['keypoints'] = self.categories
+		
+		data_coco['categories']=[categorie]
+		data_coco['annotations']=self.test_annotations
+		return data_coco	
 
 
 	def save_json(self):
 		self.data_transfer()
-		self.data_coco = self.data2coco()
-
-		json.dump(self.data_coco, open(self.save_json_path, 'w'), indent=4)  # indent=4
+		self.train_data_coco = self.train_data2coco()
+		json.dump(self.train_data_coco, open(self.train_json_path, 'w'), indent=4)  # indent=4
+		
+		self.val_data_coco = self.val_data2coco()
+		json.dump(self.val_data_coco, open(self.val_json_path, 'w'), indent=4)  # indent=4
+		
+		self.test_data_coco = self.test_data2coco()
+		json.dump(self.test_data_coco, open(self.test_json_path, 'w'), indent=4)  # indent=4
 
 # convert train data set json files
-labelme_json=glob.glob(r'G:\squidtap\keypoint1\*.json')
-labelme2coco(labelme_json,'squidtap_train.json')
+labelme_json=glob.glob(r'G:\squidtap\json\*.json')
+#create image dir
+if not os.path.exists(r'G:\\squidtap\images'):
+	os.makedirs('G:\\squidtap\\images')
+
+if not os.path.exists(r'G:\squidtap\images\train2017'):
+	os.makedirs('G:\\squidtap\\images\\train2017')
+
+if not os.path.exists(r'G:\squidtap\images\val2017'):
+	os.makedirs('G:\\squidtap\\images\\val2017')
+
+if not os.path.exists(r'G:\squidtap\images\test2017'):
+	os.makedirs('G:\\squidtap\\images\\test2017')
+
+#create annotation dir
+if not os.path.exists(r'G:\squidtap\annotations'):
+	os.makedirs('G:\\squidtap\\annotations')
+	
+labelme2coco(labelme_json,'annotations\squidtap_train2017.json','annotations\squidtap_val2017.json','annotations\image_info_test2017.json')
 
 # Convert test data set json files
 #labelme_json=glob.glob(r'D:\code\agriculture\wheat\dataset\mask rcnn model\2 Labeled data\val_data\json\*.json')
